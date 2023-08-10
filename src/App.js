243242fb3934936c2a604697c7397a6ae3e3104e
LaKhWaN <lakhwanus009@gmail.com>
2023-08-10 15:13:06 -0400

    Age Predictor - React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [predictedAge, setPredictedAge] = useState("");
+  let [name, setName] = useState("");
+  let [count, setCount] = useState("");
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+  const predictAgeBtn = () => {
+    const url = "https://api.agify.io/?name=" + name;
+    Axios.get(url).then((res) => {
+      setPredictedAge(res.data.age);
+      setCount(res.data.count);
+    });
+  };
+  useEffect(() => {
+    predictAgeBtn();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Age Predictor - API Practice</h1>
+      <input onChange={handleChange} placeholder="Enter your name"></input>
+      <button onClick={predictAgeBtn} className="catFactBtn">
+        Predict 🤔
+      </button>
+      <div className="fact">
+        <h1>
+          Your Name is, <strong>{name} 😀</strong>
+        </h1>
+        <h1>
+          and I guess you are, <strong>{predictedAge} years old.</strong>
+        </h1>
+        <h1>
+          Count: <strong>{count}</strong>
+        </h1>
+      </div>
     </div>
   );
 }
